b3594835918eaaf84299937bb2d3ce3d27de41cb
haf <henrik@haf.se>
2015-11-30 21:23:55 +0100

    initial data commit

---
+++ ExpectoPatronum/Assert.fs
@@ -0,0 +1,61 @@
+﻿module Fuchu.Assert
+
+open Fuchu
+
+let throws f =
+  try
+    f ()
+    Tests.failtest "expected f to throw"
+  with e ->
+    ()
+
+let throwsc f cont =
+  try
+    f ()
+    Tests.failtest "expected f to throw"
+  with e ->
+    cont e
+
+let isNone = function
+  | None -> ()
+  | Some x -> Tests.failtestf "expected None, was Some(%A)" x
+
+let isNonef value format =
+  match value with
+  | None -> ()
+  | Some x -> Printf.kprintf Tests.failtest format x
+
+let isSome = function
+  | None -> Tests.failtestf "expected Some _, was None"
+  | Some _ -> ()
+
+let isChoice1Of2 = function
+  | Choice1Of2 _ -> ()
+  | Choice2Of2 x -> Tests.failtestf "expected Choice1Of2, was Choice2Of2(%A)" x
+
+let isChoice1Of2f value format =
+  match value with
+  | Choice1Of2 _ -> ()
+  | Choice2Of2 x -> Printf.kprintf Tests.failtest format x
+
+let isChoice2Of2 = function
+  | Choice1Of2 x -> Tests.failtestf "expected Choice2Of2 _, was Choice1Of2(%A)" x
+  | Choice2Of2 _ -> ()
+
+let isChoice2Of2f value format =
+  match value with
+  | Choice1Of2 x -> Printf.kprintf Tests.failtest format x
+  | Choice2Of2 _ -> ()
+
+let isNotNull = function
+  | null -> Tests.failtest "expected not null, but was null"
+  | x -> ()
+
+let isNull = function
+  | null -> ()
+  | x -> Tests.failtestf "expected null, but was %A" x
+
+let isNullf value format =
+  match value with
+  | null -> ()
+  | x -> Printf.kprintf Tests.failtest format x
---
+++ ExpectoPatronum/Patronus.fsx
@@ -0,0 +1,22 @@
+﻿#I "bin/Release"
+#r "Fuchu.dll"
+#load "Assert.fs"
+open Fuchu
+
+Assert.throws <| fun _ -> Assert.isNone (Some 2)
+Assert.isNone None
+Assert.throws <| fun _ -> Assert.isNonef (Some 1) ", to hold dear %i"
+Assert.throwsc <| fun _ -> Assert.isNone (Some 1)
+               <| fun err -> ()
+
+Assert.throws <| fun _ -> Assert.isChoice1Of2 (Choice2Of2 2)
+Assert.isChoice1Of2 (Choice1Of2 3)
+
+Assert.throws <| fun _ -> Assert.isChoice2Of2 (Choice1Of2 1)
+Assert.isChoice2Of2 (Choice2Of2 111)
+
+Assert.isNull null
+Assert.throws <| fun _ -> Assert.isNull (box 2)
+Assert.throws <| fun _ -> Assert.isNullf (box 2) "expected null, got %O"
+Assert.isNotNull (box 2)
+Assert.throws <| fun _ -> Assert.isNotNull null
